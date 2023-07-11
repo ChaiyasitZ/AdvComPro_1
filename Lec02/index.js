365bce2e85b336f0d4be9b858fc89e0fd4d32433
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-11 08:45:46 +0700

    test

---
+++ Lec02/index.js
@@ -0,0 +1,77 @@
+// const people = ['John', 'Beth', 'Mike']
+// const one = 1;
+// const str = 'Hello World';
+// const employee = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+// };
+
+// function sayHello(person) {
+//     console.log('Hello ' + person.firstName);
+// }
+
+// console.log(typeof people);
+// console.log(typeof sayHello);
+// console.log(employee instanceof Array);
+// sayHello(employee);
+
+// const message = "Hello";
+// const place = "World";
+
+// // logging out a string
+// console.log('Hello, World!');
+
+// // using substitution
+// console.log('Hello, %s!', place);
+
+// // using a string literal
+// console.log(`${message}, ${place}!`);
+
+// var Hello = "Hello"; // var Hello = "Hello";
+
+// console.log(Hello); // console.log(Hello);
+// Hello = "Hello World"; // Hello = "Hello World";
+// console.log(Hello); // console.log(Hello);
+
+// if (true) { // if (true) {
+//     let World = "Hello World"; // let World = "Hello World";
+//     console.log(World); // console.log(World);
+// }
+
+// const fixval = "Hello World"; // const fixval = "Hello World";
+// console.log(fixval); // fixval = "test assign value to const"
+
+// // Concatenation with template literals
+// console.log('\n********************** Concatenation with template literals **********************\n');
+
+// let str1 = "JavaScript";
+// let str2 = "fun";
+
+// console.log('I am writing code in ${str1}');
+// console.log('Formatting in ${str1} is ${str2}!');
+
+// // Expressions in template literal
+// console.log('\n********************** Expressions in template literal **********************\n');
+
+// let bool1 = true;
+// console.log(`1 + 1 is ${1 + 1}`);
+// console.log(`The opposite of true is ${!bool1}`);
+
+// function getVowels(str) {
+//     const m = str.match(/[aeiou]/gi);
+//     if (m === null) {
+//         return 0;
+//     }
+//     return m.length;
+// }
+// console.log(getVowels('seeing'));
+
+// function test(t) {
+//     if (t === undefined) {
+//         return 'Undefined value!';
+//     }
+//     return t;
+// }
+
+// let x;
+// console.log(test(x));
