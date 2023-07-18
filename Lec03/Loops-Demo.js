3bf6d04065de5abe4f3674dab61476c229cd3fc5
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-18 10:23:53 +0700

    test

--- Lec03/Loops-Demo.js
+++ Lec03/Loops-Demo.js
@@ -3,31 +3,31 @@ const names = ['Justin', 'Sarah', 'Christopher'];
 // while loop
 console.log('\n********** while loop **********');
 let index = 0;
-while (index < names.length) {
-    const name = names[index];
-    console.log(name);
-    index++;
+while (index < names.length) { // while index is less than the length of the array
+    const name = names[index]; // get the name at the current index
+    console.log(name); // print the name
+    index++; // increment the index
 }
 
 // for loop
-console.log('\n********** for loop **********');
-for (let index = 0; index < names.length; index++) {
-    const name = names[index];
-    console.log(name);
+console.log('\n********** for loop **********'); 
+for (let index = 0; index < names.length; index++) { // for each index in the array
+    const name = names[index]; // get the name at the current index
+    console.log(name); // print the name
 }
 
 // for-of loop
 console.log('\n********** for-of loop **********');
-for (let name of names) {
-    console.log(name);
+for (let name of names) { // for each name in the array
+    console.log(name); // print the name
 }
 
 // do-while loop
 console.log('\n********** do-while loop **********');
-let index2 = 0;
-do {
-    const name = names[index2];
-    console.log(name);
-    index2++;
+let index2 = 0; // initialize the index
+do { // do the following
+    const name = names[index2]; // get the name at the current index
+    console.log(name); // print the name
+    index2++; // increment the index
 }
-while (index2 < names.length);
+while (index2 < names.length); // while index is less than the length of the array
