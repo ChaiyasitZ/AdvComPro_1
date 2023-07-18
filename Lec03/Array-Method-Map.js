5238c9a911789aa74e1e79c4ec2a4acdc716935e
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-18 09:32:45 +0700

    test

--- Lec03/Array-Method-Map.js
+++ Lec03/Array-Method-Map.js
@@ -1,8 +1,18 @@
-const finalParticipants = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
+// const finalParticipants = ['Taylor', 'Donald', 'Don', 'Natasha', 'Bobby'];
 
-const announcements = finalParticipants.map(member => {
-    return member + ' joined the contest.';
-    });
+// const announcements = finalParticipants.map(member => {
+//     return member + ' joined the contest.';
+//     });
 
-console.log(announcements);
+// console.log(announcements);
 
+const kvArray = [
+    {key: 1, value: 10},
+    {key: 2, value: 20},
+    {key: 3, value: 30}
+];
+
+const reformattedArray = kvArray.map(({key, value}) => ({[key]: value}));
+
+console.log(reformattedArray); // [{1: 10}, {2: 20}, {3: 30}]
+console.log(kvArray); // [{key: 1, value: 10}, ...]
---
+++ Lec03/jsonDemo.js
@@ -0,0 +1,14 @@
+/*************************************
+ * JavaSript for Beginners
+ * JS101 - JSON Demo
+ * 
+ * BASICS    : JSON
+ * STRINGIFY : JavaScript to JSON
+ * PARSE     : JSON to JavaScript
+ * 
+ *************************************/
+
+// Think of a simple JavaScript object with data (property values)
+const book = new Object({title: '1984', author: 'George Orwell'});
+console.log(typeof book);
+console.log(book);
