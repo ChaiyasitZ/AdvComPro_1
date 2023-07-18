1d34a714d631618dced2a5001185ebde3e519cda
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-18 08:55:07 +0700

    test

---
+++ Lec03/Array-Method-Reduce.js
@@ -0,0 +1,14 @@
+// const arrayOfNumbers = [1, 2, 3, 4];
+
+// const sum = arrayOfNumbers.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue;
+// });
+ 
+// console.log(sum); // 10
+
+const array = [15, 16, 17, 18];
+
+function reduce(accumulator, currentValue, index){
+    const return = accumulator={}
+} 
+  
