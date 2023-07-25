1561913f8567640883a4730f9bc7429374b33e47
Chaiyasit Mintakorn <s6506022610014@email.kmutnb.ac.th>
2023-07-25 09:46:55 +0700

    test

--- Lec04/exinput.js
+++ Lec04/exinput.js
@@ -1,5 +1,4 @@
-// Program to calculate BMI
-// Input: weight and height
+const { reverse } = require('dns');
 
 const rl = require('readline').createInterface({
     input: process.stdin,
@@ -8,16 +7,15 @@ const rl = require('readline').createInterface({
 
 var name, weight, height, bmi;
 
-// promt user to enter name, weight, and height
+// prompt user to enter name, weight, and height
 rl.question('Enter your name: ', (answer) => {
     name = answer;
     rl.question('Enter your weight (in kg): ', (answer) => {
-        weight = answer;
+        weight = parseFloat(answer);
         rl.question('Enter your height (in m): ', (answer) => {
-            height = answer;
-            bmi = weight / (height * height);
-            // display the result in 2 decimal places
-            console.log(`${name}, your BMI is ${bmi}`);
+            height = parseFloat(answer);
+            bmi = (weight / ((height * height)/10000)).toFixed(2); // toFixed(2) rounds to 2 decimal places
+            console.log(`${name}, your BMI is ${bmi}`); 
             rl.close();
         });
     });
--- Others/index.js
+++ Others/index.js
@@ -1,26 +1,37 @@
-function calculateBMI() {
-  const weight = parseFloat(document.getElementById('weight').value);
-  const height = parseFloat(document.getElementById('height').value);
+function calculateBMI() { 
+    var weight = parseFloat(document.getElementById('weight').value); 
+    var height = parseFloat(document.getElementById('height').value);
+    var bmi = weight / ((height/100) * (height/100));
+    var resultContainer = document.getElementById('resultContainer');
 
-  if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-      document.getElementById('result').innerText = "Please enter valid values for weight and height.";
-      return;
+    if (!isNaN(bmi)) {
+      var bmiText = "Your BMI is: " + bmi.toFixed(2);
+
+      resultContainer.innerHTML = bmiText;
+      resultContainer.classList.remove("low", "normal", "high", "caution");
+
+      if (bmi < 18.5) {
+        resultContainer.classList.add("low", "caution");
+        var cautionText = "Caution: Low BMI may indicate an eating disorder, malnutrition or other health problems.";
+        var cautionElement = document.createElement("p");
+        cautionElement.textContent = cautionText;
+        resultContainer.appendChild(cautionElement);
+      } else if (bmi >= 18.5 && bmi <= 24.9) {
+        resultContainer.classList.add("normal");
+      } else {
+        resultContainer.classList.add("high", "caution");
+        var cautionText = "Caution: High BMI may indicate increased health risks.";
+        var cautionElement = document.createElement("p");
+        cautionElement.textContent = cautionText;
+        resultContainer.appendChild(cautionElement);
+      }
+    } else {
+      resultContainer.innerHTML = "Please enter valid values for weight and height.";
+    }
   }
 
-  const bmi = weight / ((height / 100) ** 2);
-  const bmiCategory = getBMICategory(bmi);
-
-  document.getElementById('result').innerText = `Your BMI: ${bmi.toFixed(2)} - ${bmiCategory}`;
-}
-
-function getBMICategory(bmi) {
-  if (bmi < 18.5) {
-      return "Underweight";
-  } else if (bmi >= 18.5 && bmi < 24.9) {
-      return "Normal weight";
-  } else if (bmi >= 25 && bmi < 29.9) {
-      return "Overweight";
-  } else {
-      return "Obese";
-  }
-}
+  var form = document.getElementById('bmiForm');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    calculateBMI();
+  });
