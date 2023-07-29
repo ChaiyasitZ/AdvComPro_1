a1639dfa74975ef7e622e363d1a816cdfb2d1f08
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-29 22:54:21 +0700

    test

--- Project/index.js
+++ Project/index.js
@@ -1,20 +1,22 @@
-document.getElementById("calculateBtn").addEventListener("click", function() {
-    var weight = parseFloat(document.getElementById("weight").value);
+// Description: This file contains the JavaScript code for the BMI calculator.
+
+document.getElementById("calculateBtn").addEventListener("click", function() { // Add event listener to calculate button
+    var weight = parseFloat(document.getElementById("weight").value); // Convert weight from string to float
     var height = parseFloat(document.getElementById("height").value) / 100; // Convert height from cm to meters
 
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        document.getElementById("result").innerText = "Please enter valid weight and height.";
-        return;
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) { // Check if weight or height is not a number or less than or equal to 0
+        document.getElementById("result").innerText = "Please enter valid weight and height."; // Display error message
+        return; 
     }
 
-    var bmi = weight / (height * height);
-    var bmiCategory;
+    var bmi = weight / (height * height); // Calculate BMI
+    var bmiCategory; // Declare variable to store BMI category
 
-    if (bmi < 18.5) {
+    if (bmi < 18.5) { 
         bmiCategory = "Underweight";
         document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
         document.getElementById("result").style.color = "red"; // Change text color to red
-    } else if (bmi >= 18.5 && bmi < 24.9) {
+    } else if (bmi >= 18.5 && bmi < 24.9) { 
         bmiCategory = "Normal weight";
         document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
         document.getElementById("result").style.color = "green"; // Change text color to green
