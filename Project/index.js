f3d3ea3fa70595627906889b794a7372d0947eaa
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-07-29 23:23:43 +0700

    test

--- Project/index.js
+++ Project/index.js
@@ -1,33 +1,40 @@
-// Description: This file contains the JavaScript code for the BMI calculator.
+document.getElementById("calculateBtn").addEventListener("click", function() {
+    var weight = parseFloat(document.getElementById("weight").value);
+    var height = parseFloat(document.getElementById("height").value) / 100;
 
-document.getElementById("calculateBtn").addEventListener("click", function() { // Add event listener to calculate button
-    var weight = parseFloat(document.getElementById("weight").value); // Convert weight from string to float
-    var height = parseFloat(document.getElementById("height").value) / 100; // Convert height from cm to meters
-
-    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) { // Check if weight or height is not a number or less than or equal to 0
-        document.getElementById("result").innerText = "Please enter valid weight and height."; // Display error message
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+        document.getElementById("result").innerText = "Please enter valid weight and height.";
+        document.getElementById("result").style.color = "red";
         return; 
     }
 
-    var bmi = weight / (height * height); // Calculate BMI
-    var bmiCategory; // Declare variable to store BMI category
+    var bmi = weight / (height * height);
+    var bmiCategory;
 
     if (bmi < 18.5) { 
         bmiCategory = "Underweight";
         document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
-        document.getElementById("result").style.color = "red"; // Change text color to red
+        document.getElementById("result").style.color = "red";
+        document.getElementById("caution").innerText = "Caution: You are in the Underweight category. It's important to maintain a healthy weight.";
+        document.getElementById("caution").style.color = "red";
     } else if (bmi >= 18.5 && bmi < 24.9) { 
         bmiCategory = "Normal weight";
         document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
-        document.getElementById("result").style.color = "green"; // Change text color to green
+        document.getElementById("result").style.color = "green";
+        document.getElementById("caution").innerText = "Caution: You are in the Normal weight category. It's important to maintain a healthy weight.";
+        document.getElementById("caution").style.color = "green";
     } else if (bmi >= 25 && bmi < 29.9) {
         bmiCategory = "Overweight";
         document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
-        document.getElementById("result").style.color = "orange"; // Change text color to orange
+        document.getElementById("result").style.color = "orange";
+        document.getElementById("caution").innerText = "Caution: You are in the Overweight category. It's important to maintain a healthy weight.";
+        document.getElementById("caution").style.color = "orange";
     } else {
         bmiCategory = "Obese";
+        document.getElementById("result").innerText = "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("caution").innerText = "Caution: You are in the Obese category. Please consult a healthcare professional.";
+        document.getElementById("caution").style.color = "red";
     }
 
 });
-
-
