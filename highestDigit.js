3cfce677f5d7614d5a2dca2307b71e427435ac6f
Chaiyasit Mintakorn <s6506022610014@email.kmutnb.ac.th>
2023-08-22 08:38:41 +0700

    lastest

--- highestDigit.js
+++ highestDigit.js
@@ -1,13 +1,13 @@
 function highestDigit(num) {
-    let max = 0;
+    let numMax = 0;
     while (num > 0) {
         let digit = num % 10; 
-        if (digit > max) {
-            max = digit;
+        if (digit > numMax) {
+            numMax = digit;
         }
         num = Math.floor(num / 10);
     }
-    return max;
+    return numMax;
 }
 
 console.log(highestDigit(379)); 
